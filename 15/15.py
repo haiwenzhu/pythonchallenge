df7fc07b0d208d09506619d2c37244beefc13a0b
haiwenzhu <bugwhen@gmail.com>
2013-11-29 19:20:20 +0800

    solution for level 15

--- 15/15.py
+++ 15/15.py
@@ -1,1 +1,12 @@
 #http://www.pythonchallenge.com/pc/return/uzi.html
+
+from datetime import date
+
+first = second = ''
+if __name__ == '__main__':
+    for year in range(1006, 1997):
+        if year % 10 == 6 and date(year, 1, 27).weekday() == 1:
+            if (year%4 == 0 and year%100 != 0) or year%400 == 0:
+                second = first
+                first = year
+print(second)
